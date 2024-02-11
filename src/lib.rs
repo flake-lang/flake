9f60d2293e71c9508745a9623ea8c3816a5fe152
Joscha Egloff <joscha.egloff@pm.me>
2024-02-11 11:49:41 +0100

    Fixed lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,22 @@
-#[cfg(target_feature = "ast")]
+//! Flake Language
+
+#[allow(dead_code)]
+
+/// The Flake Compiler version.
+const FLAKEC_VERSION: &'static str = env!("CARGO_PKG_VERSION");
+
+#[cfg(feature = "ast")]
+/// Abstract Syntax Tree.
 pub mod ast;
-#[cfg(target_feature = "llvm-codegen")]
+#[cfg(feature = "llvm-codegen")]
+/// LLVM Code-generation.
 pub mod codegen;
-#[cfg(target_feature = "eval")]
+#[cfg(feature = "eval")]
+/// Compile-time Evaluation for Flake.
 pub mod eval;
-#[cfg(target_feature = "lexer")]
+#[cfg(feature = "lexer")]
+/// Flake's Lexer.
 pub mod lexer;
 
+/// Token Types.
 pub mod token;
